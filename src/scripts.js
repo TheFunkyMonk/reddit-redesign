e9a97d74a908888e7a92e956039d427e9b09dfdb
R.J. LaCount <rjlacount@gmail.com>
2018-07-26 11:11:31 -0700

    Add custom fonts

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,13 @@
+let styleNode = document.createElement ("style");
+styleNode.type = "text/css";
+styleNode.textContent = "@font-face { font-family: Domine; src: url('"
+	+ chrome.extension.getURL ("fonts/Domine-Regular.ttf")
+	+ "'); }\n"
+	+ "@font-face { font-family: Open Sans; src: url('"
+	+ chrome.extension.getURL ("fonts/OpenSans-Regular.ttf")
+	+ "'); }";
+document.head.appendChild (styleNode);
+
 let state = { active: true };
 
 let setClasses = (state) => {
