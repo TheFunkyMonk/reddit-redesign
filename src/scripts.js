8e7fa33f19fe349fc9b1e8e468405fa8dfd774dc
R.J. LaCount <rjlacount@gmail.com>
2018-07-26 09:48:27 -0700

    Add active state

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,18 @@
 let state = { active: true };
+
+let setClasses = (state) => {
+	let classes = document.body.classList;
+	state.active ? classes.add('redesign') : classes.remove('redesign');
+}
+
+setClasses(state);
 chrome.runtime.sendMessage(state);
+
+setTimeout(() => {
+	document.body.classList.add('loaded');
+});
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 	state = message;
-	console.log('state toggled ', state);
+	setClasses(state);
 });
